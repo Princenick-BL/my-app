745fd6f59e24bdf4a5c931937ff2065958db23bf
Nick <princenickballo@gmail.com>
2021-07-20 16:43:05 +0200

    update

--- app/components/utills/caroussel.js
+++ app/components/utills/caroussel.js
@@ -4,37 +4,37 @@ export default class caroussel extends Component {
     render() {
         return (
             
-<div class="untitled">
-	<div class="untitled__slides">
-		<div class="untitled__slide">
-			<div class="untitled__slideBg"></div>
-			<div class="untitled__slideContent">
-				<span>London</span> 
-				<span>Scout</span>
-				<a class="button" href="https://unsplash.com/@scoutthecity" target="/black">Unsplash Profile</a>
-			</div>
-		</div>
-		<div class="untitled__slide">
-			<div class="untitled__slideBg"></div>
-			<div class="untitled__slideContent">
+			<div className="untitled">
+				<div className="untitled__slides">
+					<div className="untitled__slide">
+						<div className="untitled__slideBg"></div>
+						<div className="untitled__slideContent">
+							<span>London</span> 
+							<span>Scout</span>
+							<a className="button" href="https://unsplash.com/@scoutthecity" target="/black">Unsplash Profile</a>
+						</div>
+					</div>
+					<div className="untitled__slide">
+						<div className="untitled__slideBg"></div>
+						<div className="untitled__slideContent">
+							
+							<span>Vladimir</span> 
+							<span>Kudinov</span>
+							<a className="button" href="https://unsplash.com/@madbyte" target="/black">Unsplash Profile</a>
+						</div>
+					</div>
+					<div className="untitled__slide">
+						<div className="untitled__slideBg"></div>
+						<div className="untitled__slideContent">
+							<span>Macio</span> 
+							<span>Amorim</span>
+							<a className="button" href="https://unsplash.com/@maicoamorim" target="/black">Unsplash Profile</a>
+						</div>
+					</div>
 				
-				<span>Vladimir</span> 
-				<span>Kudinov</span>
-				<a class="button" href="https://unsplash.com/@madbyte" target="/black">Unsplash Profile</a>
+				</div>
+				<div className="untitled__shutters"></div>
 			</div>
-		</div>
-		<div class="untitled__slide">
-			<div class="untitled__slideBg"></div>
-			<div class="untitled__slideContent">
-				<span>Macio</span> 
-				<span>Amorim</span>
-				<a class="button" href="https://unsplash.com/@maicoamorim" target="/black">Unsplash Profile</a>
-			</div>
-		</div>
-	
-	</div>
-	<div class="untitled__shutters"></div>
-</div>
         )
     }
 }
